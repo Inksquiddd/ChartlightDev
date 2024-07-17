250c5c66ee77c689f6c068595298d465f3824a17
ElHaBane <elhbane@uat.edu>
2024-07-16 20:39:14 -0700

    Update supervisionwindow.cpp
    for testing

--- supervisionwindow.cpp
+++ supervisionwindow.cpp
@@ -27,7 +27,7 @@ SupervisionWindow::SupervisionWindow(QWidget *parent): QWidget(parent){
 }
 
 void SupervisionWindow::deleteTask(){
-    qDebug()<<"delete task";
+    qDebug()<<"delete task. Used Git push";
 }
 
 // Button implementations
